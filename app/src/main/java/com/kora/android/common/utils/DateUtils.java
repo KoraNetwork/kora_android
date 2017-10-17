7706bf6bdf9bc3bb2a7a6ef0b69d506dfeb7a220
Volodymyr Mitso <volodymyrmitso@gmail.com>
2017-10-17 15:17:46 +0300

    Registration, date of birth

--- app/src/main/java/com/kora/android/common/utils/DateUtils.java
+++ app/src/main/java/com/kora/android/common/utils/DateUtils.java
@@ -8,8 +8,11 @@ import java.util.Locale;
 
 public class DateUtils {
 
+    private static final String PRETTY_DATE_PATTERN = "dd MMMM yyyy";
+    private static final String FORMATTED_DATE_PATTERN = "yyyy-MM-dd";
+
     public static Date getDate(String date) {
-        SimpleDateFormat mFormat = new SimpleDateFormat("YYYY-MM-dd", Locale.ENGLISH);
+        SimpleDateFormat mFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
 
         try {
             return mFormat.parse(date);
@@ -20,7 +23,7 @@ public class DateUtils {
     }
 
     public static String getPrettyDate(String date) {
-        SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM YYYY", Locale.ENGLISH);
+        SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM yyyy", Locale.ENGLISH);
 
         Date date1 = getDate(date);
 
@@ -44,15 +47,50 @@ public class DateUtils {
         return calendar;
     }
 
-    public static String getPrettyDate(int year, int monthOfYear, int dayOfMonth) {
-        SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM YYYY", Locale.ENGLISH);
+    public static String getPrettyDate(final int year, final int month, final int day) {
+        return getDate(PRETTY_DATE_PATTERN, year, month, day);
+    }
 
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, monthOfYear);
-        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+    public static String getFormattedDate(final int year, final int month, final int day) {
+        return getDate(FORMATTED_DATE_PATTERN, year, month, day);
+    }
 
-        return mFormat.format(cal.getTime());
+    public static String getDate(final String pattern, final int year, final int month, final int day) {
+        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, Locale.ENGLISH);
 
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    public static boolean isDateValid(final String date) {
+
+        final Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.YEAR, -18);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        final Date validDate = calendar.getTime();
+
+        final Date userDate;
+        try {
+            userDate = new SimpleDateFormat(FORMATTED_DATE_PATTERN, Locale.ENGLISH).parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        if (userDate.equals(validDate))
+            return true;
+        else if (userDate.before(validDate))
+            return true;
+        else if (userDate.after(validDate))
+            return false;
+
+        return false;
     }
 }
