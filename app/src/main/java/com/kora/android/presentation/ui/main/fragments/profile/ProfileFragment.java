5e65d37587dbad1b95f297208823a4857e3019f6
vladimir <vmartynyuk@petersonapps.com>
2017-10-17 16:51:44 +0300

    fix crash with null entity

--- app/src/main/java/com/kora/android/presentation/ui/main/fragments/profile/ProfileFragment.java
+++ app/src/main/java/com/kora/android/presentation/ui/main/fragments/profile/ProfileFragment.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
